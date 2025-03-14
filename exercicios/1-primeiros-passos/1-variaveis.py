cde881e323868e1734969d81f670ac87e88da967
danielapemendes <danielapmendes@outlook.pt>
2025-03-14 16:25:05 +0000

    variaveis_resolucao

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,9 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Daniela"
 # Declare variáveis de cada tipo numérico
-
+ano_nascimento = 1999
 
 # Declare uma variável do tipo booleano
+genero_feminino = True
